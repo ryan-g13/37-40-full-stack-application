4176c4cfc05cc3528fece73715ce281922eac9e4
Ryan Groesch <ryan.groesch@gmail.com>
2018-06-11 15:53:20 -0700

    Validation for sign up is now included.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Lab 37 
+# Lab 37 - 39 
 Author: Ryan Groesch
-Version 1.2.0
+Version 1.3.0
 
 # Overview
 This application has a fully functional backend that allows the use of a pubcrawl application that can geolocate you and suggest a route to follow based on your price and number of stops. 
@@ -21,6 +21,21 @@ Provider
   * If the user is not logged in they should be forced to remain on the landing route(s)
   * If the user is logged in they should not permitted to remain on the landing route(s)
 
+# Built with: 
+## Front End 
+- React 
+- Redux
+- HTML5, CSS3 
+
+## Back End
+- Node.js 
+- Express.js
+- MongoDB (Mongoose)
+- GraphHopper API
+- Twilio API
+- Google Places API 
+
 # Changes
 6/5/2018 - skelton completed and basic form functionality
 6/6/2018 - cookies for user sessions have been implemented 
+6/7/2018 - Validation included for the sign-up form.
--- frontend/src/components/auth-form/auth-form.js
+++ frontend/src/components/auth-form/auth-form.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import validator from 'validator';
 import autoBind from './../../utils';
 
 const defaultState = {
   username: '',
+  usernameDirty: false,
+  usernameError: 'Username is a Required Field',
+
   password: '',
+  passwordDirty: false,
+  passwordError: 'Password is a Required Field',
+  
   email: '',
+  emailDirty: false,
+  emailError: 'Email is a Required Field',
 };
+
+const MIN_USERNAME_LENGTH = 4;
+const MIN_PASSWORD_LENGTH = 4;
+const MAX_USERNAME_LENGTH = 32;
+const MAX_PASSWORD_LENGTH = 16;
 
 class AuthForm extends React.Component {
   constructor(props) {
@@ -17,13 +31,72 @@ class AuthForm extends React.Component {
 
   handleChange(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    this.setState({ 
+      [name]: value,
+      [`${name}Dirty`]: true,
+      [`${name}Error`]: this.handleValidation(name, value), 
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.onComplete(this.state);
-    this.setState(defaultState);
+    const { usernameError, emailError, passwordError } = this.state;
+    if (this.props.type === 'login' || (!usernameError && !passwordError && !emailError)) {
+      this.props.onComplete(this.state);
+      this.setState(defaultState);
+    } else {
+      this.setState({
+        usernameDirty: true,
+        passwordDirty: true,
+        emailDirty: true,
+      });
+    }
+  }
+
+  handleValidation(name, value) {
+    if (this.props.type === 'login') {
+      return null;
+    }
+    switch (name) {
+      case 'username':
+        if (value.length < MIN_USERNAME_LENGTH) {
+          return `Your username must be ${MIN_USERNAME_LENGTH} characters in length.`;
+        }
+        if (value.length > MAX_USERNAME_LENGTH) {
+          return `Your username must be less than ${MAX_USERNAME_LENGTH} characters in length.`;
+        }
+        return null;
+      
+      case 'password': 
+        if (value.length < MIN_PASSWORD_LENGTH) {
+          return `Your password must be ${MIN_PASSWORD_LENGTH} characters in length.`;
+        }
+        if (value.length > MAX_PASSWORD_LENGTH) {
+          return `Your password must be less than ${MAX_PASSWORD_LENGTH} characters in length.`;
+        }
+        if (!/(?=[a-z])/.test(value)) {
+          return 'Your password must contain a lowercase letter.';
+        } 
+        if (!/(?=[A-Z])/.test(value)) {
+          return 'Your password must contain an Uppercase letter.';
+        }
+        if (!/(?=\d)/.test(value)) {
+          return 'Your password must contain a numerical digit.';
+        }
+        if (!/(?=\W)/.test(value)) {
+          return 'Your password must contain a special character !@#$%^&*().';
+        }
+        return null;
+
+      case 'email':
+        if (!validator.isEmail(value)) {
+          return 'You must provide a valid Email';
+        }
+        return null;
+
+      default: 
+        return null;
+    }
   }
 
   render() {
@@ -31,18 +104,22 @@ class AuthForm extends React.Component {
     type = type === 'login' ? type : 'signup';
     
     const signupJSX = 
+      <div>
+        { this.state.emailDirty ? <p> {this.state.emailError }</p> : undefined }
       <input 
         name='email'
         placeholder='Enter Email Here'
         type='email'
         value={this.state.email}
         onChange={this.handleChange}
-      />;
+      />
+      </div>;
 
     const signupRenderedJSX = (type !== 'login') ? signupJSX : undefined;
 
     return (
       <form className='authForm' onSubmit={this.handleSubmit}>
+        { this.state.usernameDirty ? <p>{ this.usernameError }</p> : undefined }
         <input 
           name='username'
           placeholder='Enter Username Here'
@@ -51,7 +128,9 @@ class AuthForm extends React.Component {
           onChange={this.handleChange}
         />
 
+        { this.state.passwordDirty ? <p>{ this.state.passwordError }</p> : undefined }
         <input
+          className={ this.state.passwordDirty ? 'input-error' : '' }
           name='password'
           placeholder='Enter Password'
           type='password'
